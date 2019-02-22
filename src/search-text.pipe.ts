0b434feb675e090ca60eb34a33bb580fa427ff6a
Khang Nguyen <mkavo92@gmail.com>
2019-02-22 14:30:35 -0600

    nothing change much

--- src/search-text.pipe.ts
+++ src/search-text.pipe.ts
@@ -6,7 +6,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class SearchTextPipe implements PipeTransform {
 
-    transform(items: Array<any>, titleSearch: string, descriptionSearch: string, addressSearch: string) {
+    transform(items: Array<any>, titleSearch: string, descriptionSearch: string, addressSearch: string, dateSearch: string) {
         if (items && items.length) {
             return items.filter(item => {
                 if (titleSearch && item.title.toLowerCase().indexOf(titleSearch.toLowerCase()) === -1) {
@@ -17,6 +17,9 @@ export class SearchTextPipe implements PipeTransform {
                 }
                 if (addressSearch && item.address.toLowerCase().indexOf(addressSearch.toLowerCase()) === -1) {
                     return false;
+                }
+                if (dateSearch && item.date.toLowerCase().indexOf(dateSearch.toLowerCase()) === -1) {
+                    return false
                 }
                 return true;
             })
