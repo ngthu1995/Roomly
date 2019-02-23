d4ddf67413a03071546a2270e1b323ffd8e2a442
Thu Nguyen <ngthu1995@gmail.com>
2019-02-23 17:49:28 -0500

    abcxyz

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,19 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const UserController = require("../controller/auth_controller");
-const passport = require('../middleware/passport')
+const passport = require("../middleware/passport");
 
 // router.get('/:id', UserController.authMiddleware, UserController.getUser)
 
 router.post("/login", UserController.loginAuth);
-<<<<<<< HEAD
-router.post("/register", UserController.register);
-=======
 router.post("/register", UserController.getUser);
 
-router.post('/checkManager', UserController.confirmManager);
-router.get('/list', UserController.getUsers);
->>>>>>> cdd393ce8eacf64d2703fd1e59910035393fe184
+router.get("/list", UserController.getUsers);
 
-router.post('/checkManager', UserController.confirmManager)
+router.post("/checkManager", UserController.confirmManager);
 module.exports = router;
